b272717bf89f69dd2f8b736d8d57754175f8b763
dell <dell@DESKTOP-0V4VLF9>
2021-05-11 23:26:07 +0530

    Class and Obj 2nd Commit

---
+++ StaticCalci/src/SCalci/Calculator.java
@@ -0,0 +1,27 @@
+package SCalci;
+import java.util.*;
+public class Calculator {
+    Calculator()
+	{
+		int powerInt(int num1,int num2){
+			return Math.pow(num1, num2);
+		}
+	    double powerDouble(double num3,double num4){
+			return Math.pow(num3, num4);
+			}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		Calculator c = new Calculator();  
+		System.out.print("Enter integer values\n");
+		int num1=sc.nextInt();
+		int num2=sc.nextInt();
+		System.out.print("Enter double values\n");
+		double num3=sc.nextDouble();
+		double num4=sc.nextDouble();
+		System.out.print("Power of Integer Values = "+c.powerInt(num1, num2));
+		System.out.print("\nPower of Double Values = "+c.powerDouble(num3, num4));
+	}
+}
