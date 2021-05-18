4664ff3cfaf64073d7d993dfbb5f6812388a871a
dell <dell@DESKTOP-0V4VLF9>
2021-05-18 22:11:35 +0530

    CMD1 Commit

---
+++ CMD_1/src/Program_1.java
@@ -0,0 +1,22 @@
+package CMD_1;
+
+import java.util.Scanner;
+
+import java.util.*;
+import java.lang.*;
+public class Program_1 {
+	public static void main(String args[])
+	{
+		String str1=args[0];
+		String str2=args[1];
+		if(str1=="Wipro")
+		{
+			System.out.print("Wipro Technologies "+str2);
+		}
+		else
+		{
+			System.out.print(str1+" Technologies "+str2);
+		}
+	}
+
+}
