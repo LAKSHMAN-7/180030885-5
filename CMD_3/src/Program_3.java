941b89ddb484808f02ce0ba9e1b584aa98963877
dell <dell@DESKTOP-0V4VLF9>
2021-05-18 22:17:27 +0530

    CMD3 Commit

---
+++ CMD_3/src/Program_3.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Program_3 {
+	public static void main(String args[])
+	{
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		System.out.print("The sum of "+a+" and "+b+" is " +(a+b));
+	}
+
+}
