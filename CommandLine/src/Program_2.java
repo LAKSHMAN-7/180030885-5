a10eb8598c321ee321b880e0330fc8c3ed2ed126
dell <dell@DESKTOP-0V4VLF9>
2021-05-18 22:18:13 +0530

    CMD2 Commit

---
+++ CommandLine/src/Program_2.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Program_2 {
+	public static void main(String args[])
+	{
+		String str1=args[0];
+		System.out.print("Welcome "+str1);
+	}
+}
